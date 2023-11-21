ef3ffaabd313d6031354e149682a55257fabb876
Suizzle <andygolay@gmail.com>
2023-11-21 12:16:13 -0500

    Create keygen.ts

---
+++ keygen.ts
@@ -0,0 +1,6 @@
+import { Keypair } from "@solana/web3.js";
+import bs58 from 'bs58';
+import * as prompt from 'prompt-sync';
+// Generate a new keypair
+let kp = Keypair.generate();
+console.log(`You've generated a new Solana wallet: ${kp.secretKey} ${kp.publicKey.toBase58()}`);
