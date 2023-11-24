edae797638984648da14662ecca00cbc6de40a61
Suizzle <andygolay@gmail.com>
2023-11-24 16:43:13 +0000

    added cluster1 files

---
+++ cluster1/nft_mint.ts
@@ -0,0 +1,36 @@
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
+import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi"
+import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
+
+import wallet from "../wba-wallet.json"
+import base58 from "bs58";
+
+const RPC_ENDPOINT = "https://api.devnet.solana.com";
+const umi = createUmi(RPC_ENDPOINT);
+
+let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+const myKeypairSigner = createSignerFromKeypair(umi, keypair);
+umi.use(signerIdentity(myKeypairSigner));
+umi.use(mplTokenMetadata())
+
+const mint = generateSigner(umi);
+
+(async () => {
+    let tx = createNft(
+        umi,
+        {
+            mint: mint,
+            authority: umi.identity,
+            name: "Generug",
+            symbol: "GRUG",
+            uri: "https://arweave.net/H_H2TCLh-exqlM43viSv1c8uxM_3rpjpmyA9hvMeriw",
+            sellerFeeBasisPoints: percentAmount(0),
+        })
+
+    let result = await tx.sendAndConfirm(umi);
+    const signature = base58.encode(result.signature);
+
+    console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+
+    console.log("Mint Address: ", mint.publicKey);
+})();
---
+++ cluster1/spl_metadata.ts
@@ -0,0 +1,57 @@
+import wallet from "../wba-wallet.json"
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
+import {
+    createMetadataAccountV3,
+    CreateMetadataAccountV3InstructionAccounts,
+    CreateMetadataAccountV3InstructionArgs,
+    DataV2Args
+} from "@metaplex-foundation/mpl-token-metadata";
+import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-foundation/umi";
+
+// Define our Mint address
+const mint = publicKey("9xXa31Mnqxsnhd27uz4UTyrztzHA2f15bqb2dJ3puPhD")
+
+// Create a UMI connection
+const umi = createUmi('https://api.devnet.solana.com');
+const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+const signer = createSignerFromKeypair(umi, keypair);
+umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
+(async () => {
+    try {
+
+        let accounts: CreateMetadataAccountV3InstructionAccounts = {
+            mint,
+            mintAuthority: signer
+        }
+
+        let data: DataV2Args = {
+            name: "Suizzle Coin",
+            symbol: "SUIZ",
+            uri: "",
+            sellerFeeBasisPoints: 0,
+            collection: null,
+            creators: null,
+            uses: null
+        }
+
+        let args: CreateMetadataAccountV3InstructionArgs = {
+            data,
+            isMutable: true,
+            collectionDetails: null,
+        }
+
+        const tx = createMetadataAccountV3(
+            umi,
+            {
+                ...accounts,
+                ...args,
+            }
+        )
+
+        let result = await tx.sendAndConfirm(umi);
+
+        console.log(result.signature);
+    } catch (e) {
+        console.error(`Oops, something went wrong: ${e}`)
+    }
+})();
