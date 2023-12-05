be9ac8a6723f29d8a972e6b13d965a9815eb2cf9
suizzle <andygolay@gmail.com>
2023-12-05 15:45:03 -0500

    Completed SPL transfer (forgot to do apparently)

--- cluster1/spl_transfer.ts
+++ cluster1/spl_transfer.ts
@@ -10,18 +10,24 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("9R69N8GnLkbo76q3oVM5xPMi5Spkz7hmKAvpZMpd7t2h");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("HM2AULAh4HaD1ACdkpuBaF84sfRhS1zVABcG3VRh55nM");
 
 (async () => {
     try {
         // Get the token account of the fromWallet address, and if it does not exist, create it
+        // Create an ATA
+        const ata1 = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, keypair.publicKey)
 
         // Get the token account of the toWallet address, and if it does not exist, create it
+// Create an ATA
+        const ata2 = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, to)
 
         // Transfer the new token to the "toTokenAccount" we just created
+        const transferTx = await transfer(connection, keypair, ata1.address, ata2.address, keypair.publicKey, 1000000);
+        console.log(`Success! Check out your transfer tx: https://explorer.solana.com/tx/${transferTx}?cluster=devnet`);
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
