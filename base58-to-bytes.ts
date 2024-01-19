56630e42fe39c9eab6fe1c8cabb82a743dd2cb95
Suizzle <andygolay@gmail.com>
2024-01-19 16:03:44 -0500

    Delete base58-to-bytes.ts

--- base58-to-bytes.ts
+++
@@ -1,7 +0,0 @@
-const bs58 = require('bs58');
-
-const secret = '7aewfu5H3ux3RqaLrBU3FUTrNf2T8LSZsJubWtVuqG9HTyB2vFcBM14zqFAPEidznvGUcKKkBLVS9iVCZhoYoBe';
-const bytes = bs58.decode(secret);
-// See uint8array-tools package for helpful hex encoding/decoding/compare tools
-//console.log(Buffer.from(bytes).toString('hex'))
-console.log(`${bytes}`);
